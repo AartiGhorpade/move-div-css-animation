9e7c291dae331f5f3f8df727170c1ebfc5ab8347
AartiGhorpade <89845285+AartiGhorpade@users.noreply.github.com>
2024-04-29 22:03:30 +0530

    div moving animation

---
+++ move-div-css-animation/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+     <section className="cardMoveAnimation">
+        <div className="animationDiv"></div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
